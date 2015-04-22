ac4e0779cf60b4ebb97c51c17bda940f731c2792
Clint Small Cain <github@breakbottle.com>
2015-04-21 21:46:04 -0700

    angularjs and requirejs for services and factories

---
+++ JavaScript-Angularjs-RequireJS-ServicesAndFactories/AngularJs-RequireJs-Gist-For-ServicesAndFactories.js
@@ -0,0 +1,48 @@
+/**
+ * Author: Clint Small Cain
+ * Date: 4/21/2015
+ * Time: 9:34 PM
+ * Description:
+ */
+angular.module('app').factory('RequireJs',function($q){
+    //requires services and factories files when needed.
+    return function(requireList){
+        require.config({
+            baseUrl: '',
+            paths: {//add you paths
+                /*
+                 'bilDebug':['app/common/debug'],
+                 'test':['app/common/test']
+
+                 Example:
+                     Usage - cal factory name, give list of variables names, then list of services and factories to inject
+                     RequireJs(['bilDebug','test']).then(function(k){
+                     console.log("what is k", )
+                 })
+                 */
+
+            }
+        });
+        var promise = $q.defer();
+        require(requireList,function(d){
+            var inject =  angular.injector([application.name]);
+            var resolved = [];
+            if(angular.isArray(requireList)){
+                angular.forEach(requireList,function(value,key){
+                    var re = inject.get(value);
+                    if(angular.isFunction(re)){
+                        var objectIn = {};
+                        objectIn[value] = re.call();
+                        resolved.push(objectIn);
+                    } else {
+                        resolved.push(re);
+                    }
+                });
+            }
+            promise.resolve(resolved);
+        });
+        return promise.promise;
+
+    };
+
+});
